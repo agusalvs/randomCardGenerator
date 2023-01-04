0a5f043fa9029b88914f43f9ee01e0a02a27daaf
agusalvs <agustina.alvarezsacra@gmail.com>
2023-01-04 13:28:50 +0000

    Random Card Generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,49 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
+// let button = document.querySelector("#button");
+//button.addEventListener(
+//"click", (
+window.onload = setInterval(function randomCard() {
+  const palos = ["♦", "♥", "♠", "♣"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  let indexPalo = Math.floor(Math.random() * palos.length);
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let top = document.querySelector(".top");
+  top.innerHTML = palos[indexPalo];
+
+  let bottom = document.querySelector(".bottom");
+  bottom.innerHTML = palos[indexPalo];
+
+  if (palos[indexPalo] == "♥" || palos[indexPalo] == "♦") {
+    top.style.color = "red";
+  } else {
+    top.style.color = "black";
+  }
+  if (palos[indexPalo] == "♥" || palos[indexPalo] == "♦") {
+    bottom.style.color = "red";
+  } else {
+    bottom.style.color = "black";
+  }
+
+  const numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexNum = Math.floor(Math.random() * numbers.length);
+  let number = document.querySelector(".number");
+  number.innerHTML = numbers[indexNum];
+  // let bot = document.querySelector(".bot");
+  // bot = top;
+}, 10000);
